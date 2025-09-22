1a6bdbb889f7c9c1b3b8ea1fdc335ae4261e88c8
AMAN GUPTA <ag0567688@gmail.com>
2025-09-22 18:32:52 +0530

    backend

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -13,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
-
+app.use('/', (req,res)=>{
+    res.send("Welcome to AI Coach Backend");
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
